62d0fb0f15234ec2b61578ea0dacd782c5a0968b
Simon Masson <simon.masson@protonmail.com>
2025-12-29 16:27:48 +0100

    Add comments on existing deployments

--- packages/pq-account/script/DeployFixedContracts.s.sol
+++ packages/pq-account/script/DeployFixedContracts.s.sol
@@ -18,10 +18,9 @@ contract HybridVerifierFixedContract is BaseScript {
 
     function run() external returns (address) {
         uint256 chainId = block.chainid;
-        console.log(chainId);
-        if (chainId == 11155111) {
+        if (chainId == 11155111) { // deployment on Sepolia L1
             return address(0x78E229b83378DF8A9AC1164156b542eBbDE2a1D5);
-        } else if (chainId == 421614) {
+        } else if (chainId == 421614) { // deployment on Sepolia Arbitrum
             return address(0x48237092dFe6387B1d7D2AacDA42bc43EdA44aEa);
         } else {
             vm.startBroadcast();
@@ -39,9 +38,9 @@ contract MLDSAFixedContract is BaseScript {
 
     function run() external returns (address) {
         uint256 chainId = block.chainid;
-        if (chainId == 11155111) {
+        if (chainId == 11155111) { // deployment on Sepolia L1
             return address(0xc15278300d4736C10c465E0f73b2D9eCC1c0d94B);
-        } else if (chainId == 421614) {
+        } else if (chainId == 421614) { // deployment on Sepolia Arbitrum
             return address(0xbfF3cd81fDf061D002A91dE3cD589E814AfdC94a);
         } else {
             vm.startBroadcast();
@@ -58,9 +57,9 @@ contract MLDSAETHFixedContract is BaseScript {
 
     function run() external returns (address) {
         uint256 chainId = block.chainid;
-        if (chainId == 11155111) {
+        if (chainId == 11155111) { // deployment on Sepolia L1
             return address(0xa3B09eF2A08f5EF5EB1C091d41a47A39eCB87433);
-        } else if (chainId == 421614) {
+        } else if (chainId == 421614) { // deployment on Sepolia Arbitrum
             return address(0x238045D114024576bf75700aa0eCFEfb47EF764F);
         } else {
             vm.startBroadcast();
@@ -77,9 +76,9 @@ contract FALCONFixedContract is BaseScript {
 
     function run() external returns (address) {
         uint256 chainId = block.chainid;
-        if (chainId == 11155111) {
+        if (chainId == 11155111) { // deployment on Sepolia L1
             return address(0x8f44FC27b333F0064f13a8c5e3451d4f65D75E60);
-        } else if (chainId == 421614) {
+        } else if (chainId == 421614) { // deployment on Sepolia Arbitrum
             return address(0x5Ce696b0F838C70A64be9D3Ee9017f35A4CBb091);
         } else {
             vm.startBroadcast();
@@ -96,9 +95,9 @@ contract ETHFALCONFixedContract is BaseScript {
 
     function run() external returns (address) {
         uint256 chainId = block.chainid;
-        if (chainId == 11155111) {
+        if (chainId == 11155111) { // deployment on Sepolia L1
             return address(0x544F59a8Adb31818bfcFEA4759DD8495aFF2E30f);
-        } else if (chainId == 421614) {
+        } else if (chainId == 421614) { // deployment on Sepolia Arbitrum
             return address(0x8B210Cd6E66a5d6EABD50cefE8Ef66A0e5b3e7a2);
         } else {
             vm.startBroadcast();
@@ -115,9 +114,9 @@ contract ECDSAK1FixedContract is BaseScript {
 
     function run() external returns (address) {
         uint256 chainId = block.chainid;
-        if (chainId == 11155111) {
+        if (chainId == 11155111) { // deployment on Sepolia L1
             return address(0x70b7bB1CD374768Af0d2Ad76aB7EBD0Aca4b54d6);
-        } else if (chainId == 421614) {
+        } else if (chainId == 421614) { // deployment on Sepolia Arbitrum
             return address(0x51dD569c0A1be3Ed093992dc8745cf324d203bb5);
         } else {
             vm.startBroadcast();
