e930861ef8fd69b74297879c4a19e2afba9c5373
Robert MacWha <github@macwha.com>
2026-03-03 10:12:17 -0500

    tidy: remove debug changes

--- packages/railgun/src/test.ts
+++
@@ -1,9 +0,0 @@
-import { Eip155ChainId } from "@kohaku-eth/plugins";
-import { Broadcasters } from "./broadcasters";
-
-
-async function test() {
-    const broadcasters = await Broadcasters.create(new Eip155ChainId(1));
-}
-
-test()
