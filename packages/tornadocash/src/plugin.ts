6210c67bf7f98aa178b42332843cb7c6d25a3bb2
Robert MacWha <github@macwha.com>
2026-03-17 20:24:06 -0400

    wip: tornado

--- packages/tornadocash/src/plugin.ts
+++ packages/tornadocash/src/plugin.ts
@@ -1,13 +1,14 @@
-import { InsufficientBalanceError, UnsupportedAssetError, type AssetAmount, type CreatePluginFn, type Host, type PluginInstance, type PublicOperation } from '@kohaku-eth/plugins';
-import { JsNote, JsSyncer, JsTornadoProvider, pools } from 'node_modules/tc-js/dist/pkg/tc_rs';
-import { EthereumProviderAdapter } from './ethereum-provider';
+import { AssetId, InsufficientBalanceError, UnsupportedAssetError, UnsupportedChainError, type AssetAmount, type CreatePluginFn, type Host, type PluginInstance, type PublicOperation } from '@kohaku-eth/plugins';
+import { JsNote, JsPool, JsRelayerProvider, JsSyncer, JsTornadoProvider, pools } from 'node_modules/tc-js/dist/pkg/tc_rs';
 import { RemoteArtifactLoader, TornadoClassicProver } from 'tc-js';
 import { TxData } from '@kohaku-eth/provider';
+import { Broadcaster } from '@kohaku-eth/plugins/broadcaster';
+import { EthereumProviderAdapter } from './ethereum-provider';
 
-export type TornadoInstance = PluginInstance<
-    TornadoAddress,
+export type TcInstance = PluginInstance<
+    TcAddress,
     {
-        privateOp: TornadoPrivateOperation,
+        privateOp: TcPrivateOperation,
         features: {
             prepareShield: true,
             prepareUnshield: true,
@@ -15,78 +16,97 @@ export type TornadoInstance = PluginInstance<
     }
 >;
 
-export type TornadoAddress = `${string}`;
-export type TornadoPrivateOperation = {
+export type TcBroadcaster = Broadcaster<TcPrivateOperation>;
+
+export type TcAddress = `${string}`;
+export type TcPrivateOperation = {
 
 };
 
-export async function createTornadoPlugin(host: Host): TornadoInstance {
-    const provider = await newTornadoProvider(host);
-    const notes: JsNote[] = [];
+export async function createTornadoPlugin(host: Host): TcInstance {
+    return;
+}
 
-    /**
-     * Return the balance of the given asset, or all assets if `assets` is undefined.
-     */
-    async function balance(assets: TornadoAddress[] | undefined): Promise<AssetAmount[]> {
-        let balances: AssetAmount[] = [];
-        for (const note of notes) {
+export class TornadoClassicPlugin implements TcInstance, TcBroadcaster {
+    constructor(
+        private provider: JsRelayerProvider,
+        private notes: JsNote[],
+    ) { }
+
+    async balance(assets: AssetId[] | undefined): Promise<AssetAmount[]> {
+        let balances = new Map<`0x${string}`, bigint>();
+
+        for (const note of this.notes) {
             const pool = note.pool();
-            if (pool === undefined) {
+            if (pool === undefined) { continue; }
+            const asset = pool.asset;
+            if (asset.type !== "Erc20") { continue; }
+            if (assets !== undefined && !assets.some((a) => a.__type === 'erc20' && a.contract === asset.address)) {
                 continue;
             }
 
-            if (pool.asset)
+            if (!balances.has(asset.address)) {
+                balances.set(asset.address, 0n);
+            }
+            balances.set(asset.address, balances.get(asset.address)! + BigInt(note.amount));
         }
-        // let filtered: JsNote[] = [];
-        // if (assets === undefined) {
-        //     filtered = notes;
-        // } else {
-        //     filtered = notes.filter((note) => assets.includes(note.pool()?.asset as TornadoAddress));
-        // }
 
-        // filtered = filtered.filter((note) => note.pool() !== undefined);
-        // return filtered.map((note) => ({
-        //     asset: {
-        //         __type: 'erc20',
-        //         contract: note.pool()!.address,
-        //     },
-        //     amount: BigInt(note.amount),
-        // }));
+        return Array.from(balances.entries()).map(([address, amount]) => ({
+            asset: {
+                __type: 'erc20',
+                contract: address,
+            },
+            amount,
+        }));
     }
 
-    async function prepareShield(asset: AssetAmount): Promise<TxData> {
-        const pool = pools().find((p) => p.address === asset.asset.contract && BigInt(p.amount_wei) === asset.amount);
+    async prepareShield(token: AssetAmount): Promise<TxData> {
+        const pool = pools().find((p) => p.address === token.asset.contract && BigInt(p.amountWei) === token.amount);
+
+        // TODO: Consider splitting into multiple deposits?
         if (!pool) {
-            throw new UnsupportedAssetError(asset.asset);
+            console.warn(`No pool found for asset ${token.asset.contract} with amount ${token.amount}`);
+            throw new UnsupportedAssetError(token.asset);
         }
 
-        const shield = provider.deposit(pool);
-        notes.push(shield.note);
+        const shield = this.provider.deposit(pool);
+        this.notes.push(shield.note);
         return {
             to: shield.txData.to,
             data: shield.txData.data,
-            value: BigInt(shield.txData.value)
+            value: BigInt(shield.txData.value),
         }
     }
 
-    async function prepareUnshield(asset: AssetAmount, to: `0x${string}`): Promise<TornadoPrivateOperation> {
-        const note = notes.find((n) => n.pool()?.address === asset.asset && BigInt(n.amount) === asset.amount);
+    async prepareUnshield(token: AssetAmount, to: `0x${string}`): Promise<TcPrivateOperation> {
+        const note = this.notes.find((n) => {
+            const pool = n.pool();
+            const asset = pool?.asset;
+            return asset?.type == "Erc20" && asset.address === token.asset.contract && BigInt(n.amount) === token.amount;
+        });
+
+        // TODO: Consider splitting up larger notes into smaller ones.  This would 
+        // however require re-shielding the change, which adds complexity.
         if (!note) {
-            throw new InsufficientBalanceError(asset.asset, asset.amount, 0n);
+            throw new InsufficientBalanceError(token.asset, token.amount, 0n);
         }
-    }
 
-
-    return {
-        balance,
-        prepareShield,
-        instanceId: async () => "0",
-
+        const unshield = await this.provider.withdraw(note, to);
     }
 }
 
-async function newTornadoProvider(host: Host): Promise<JsTornadoProvider> {
+async function newTornadoProvider(host: Host, pool: JsPool): Promise<JsRelayerProvider> {
     const ethRpcProvider = new EthereumProviderAdapter(host.provider);
+    const chainId = await ethRpcProvider.getChainId();
+
+    // TODO: Support other chains.
+    //
+    // This will require updating the sync snapshots, 
+    if (chainId !== 1n) {
+        throw new UnsupportedChainError(chainId);
+    }
+
+    // const cacheSyncer = await JsSyncer.newCache()
     const syncer = await JsSyncer.newRpc(ethRpcProvider, 10000n);
 
     const loader = new RemoteArtifactLoader(
@@ -94,5 +114,5 @@ async function newTornadoProvider(host: Host): Promise<JsTornadoProvider> {
         "https://raw.githubusercontent.com/Robert-MacWha/privacy-protocol-artifacts/refs/heads/main/artifacts/tornadocash-classic/tornadoProvingKey.bin"
     );
     const prover = new TornadoClassicProver(loader);
-    return JsTornadoProvider.new(ethRpcProvider, syncer, prover);
+    return JsRelayerProvider.new(ethRpcProvider, syncer, prover, ethRpcProvider);
 }
