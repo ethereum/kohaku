183e47d48f3dfdfc603c477eb3a429f0ecb1eef3
Robert MacWha <github@macwha.com>
2026-03-23 13:33:02 -0400

    feat: add sync-utxo-sepolia test

--- crates/railgun-rs/tests/integration/main.rs
+++ crates/railgun-rs/tests/integration/main.rs
@@ -3,6 +3,8 @@ mod sync_txid;
 #[cfg(not(target_arch = "wasm32"))]
 mod sync_utxo;
 #[cfg(not(target_arch = "wasm32"))]
+mod sync_utxo_sepolia;
+#[cfg(not(target_arch = "wasm32"))]
 mod transact;
 #[cfg(all(not(target_arch = "wasm32"), feature = "poi"))]
 mod transact_poi;
