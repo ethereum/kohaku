a4af318b0c3f29bf499ed34b76dee91a4fa5489f
Robert MacWha <github@macwha.com>
2026-03-24 16:55:48 -0400

    fix: railgun exports (#114)

--- crates/railgun-rs/tests/integration/main.rs
+++ crates/railgun-rs/tests/integration/main.rs
@@ -3,8 +3,6 @@ mod sync_txid;
 #[cfg(not(target_arch = "wasm32"))]
 mod sync_utxo;
 #[cfg(not(target_arch = "wasm32"))]
-mod sync_utxo_sepolia;
-#[cfg(not(target_arch = "wasm32"))]
 mod transact;
 #[cfg(all(not(target_arch = "wasm32"), feature = "poi"))]
 mod transact_poi;
