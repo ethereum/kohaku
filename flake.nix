782325386bab8d9f09c6f5ee3707f445aa4cfa9e
κασσάνδρα.eth <0xDADA@protonmail.com>
2026-04-08 06:56:16 +0200

    fix: revert overly complex flake.nix additions

--- flake.nix
+++ flake.nix
@@ -34,45 +34,6 @@
         };
 
         rustfmtNightly = pkgs.rust-bin.nightly.latest.rustfmt;
-
-        # nixpkgs wasm-pack often lags crates.io; match CI (v0.14.0) via upstream binaries.
-        wasm-pack =
-          let
-            version = "0.14.0";
-            asset =
-              if system == "x86_64-linux" then {
-                id = "x86_64-unknown-linux-musl";
-                hash = "sha256-J4qNZoCFgh9NGmN72GTxcT+HKwrjoRjHdWKjCMCr/o0=";
-              }
-              else if system == "aarch64-linux" then {
-                id = "aarch64-unknown-linux-musl";
-                hash = "sha256-WUHHsFBgRA/zfuUP6QCaQI5j+lumB6Owc29aiH7F8so=";
-              }
-              else if system == "x86_64-darwin" then {
-                id = "x86_64-apple-darwin";
-                hash = "sha256-RrZgcu6ZErU/g4Qa7LBEeaYOBwX3u4tmgbN3oHpRKiM=";
-              }
-              else if system == "aarch64-darwin" then {
-                id = "aarch64-apple-darwin";
-                hash = "sha256-nQ5wxrIp3hjwq/6RDylj6PCeuuIYJQ6bCaHD/dlVvvk=";
-              }
-              else
-                throw "wasm-pack ${version}: unsupported system ${system}";
-          in
-          pkgs.stdenvNoCC.mkDerivation {
-            pname = "wasm-pack";
-            inherit version;
-            src = pkgs.fetchurl {
-              url = "https://github.com/wasm-bindgen/wasm-pack/releases/download/v${version}/wasm-pack-v${version}-${asset.id}.tar.gz";
-              hash = asset.hash;
-            };
-            sourceRoot = "wasm-pack-v${version}-${asset.id}";
-            dontConfigure = true;
-            dontBuild = true;
-            installPhase = ''
-              install -D -m755 wasm-pack $out/bin/wasm-pack
-            '';
-          };
       in
       {
         devShells = {
@@ -87,7 +48,7 @@
 
               # Wasm tools
               pkgs.binaryen
-              wasm-pack
+              pkgs.wasm-pack
               pkgs.nodejs_24
               pkgs.pnpm
               # pkgs.twiggy
@@ -112,7 +73,7 @@
               pkgs.just
               pkgs.foundry
               pkgs.binaryen
-              wasm-pack
+              pkgs.wasm-pack
               pkgs.nodejs_24
               pkgs.pnpm
             ];
