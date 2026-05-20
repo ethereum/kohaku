b1a99c574db35316abbf26c4df007c5a7be2012b
Robert MacWha <github@macwha.com>
2026-05-20 12:53:31 -0400

    feat: add per-chain database key for ts

--- crates/railgun-ts/sdk/database.ts
+++ crates/railgun-ts/sdk/database.ts
@@ -3,24 +3,26 @@ import type { Database } from "../pkg";
 
 /**
  * Adapter that wraps a kohaku database and exposes the railgun Database interface.
- * 
- * TODO: Delete me once delete is implemented in storage since we can just use storage directly.
  */
 export class DatabaseAdapter implements Database {
-    constructor(private storage: Storage) { }
+    constructor(private prefix: string, private storage: Storage) { }
 
     async get(key: string): Promise<string | null> {
-        const value = this.storage.get(key);
+        const value = this.storage.get(this.key(key));
 
         // TODO: Remove me once delete is implemented
         return value || value === "" ? value : null;
     }
 
     async set(key: string, value: string): Promise<void> {
-        this.storage.set(key, value);
+        this.storage.set(this.key(key), value);
     }
 
     async delete(key: string): Promise<void> {
-        this.storage.set(key, ""); // TODO: Add a delete method to Storage
+        this.storage.set(this.key(key), ""); // TODO: Add a delete method to Storage
+    }
+
+    private key(key: string): string {
+        return `${this.prefix}:${key}`;
     }
 }
