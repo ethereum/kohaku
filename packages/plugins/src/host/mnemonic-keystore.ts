f1f6166ece87e1bbf183daa0eb6c2841d057d654
Robert MacWha <github@macwha.com>
2026-05-22 10:51:37 -0400

    feat: add local impls for keystore and storage

---
+++ packages/plugins/src/host/mnemonic-keystore.ts
@@ -0,0 +1,31 @@
+import { Keystore } from "./index";
+import { HDKey } from "@scure/bip32";
+import { mnemonicToSeedSync, generateMnemonic } from "@scure/bip39";
+import { wordlist } from "@scure/bip39/wordlists/english.js";
+import { Hex } from "ox";
+
+/**
+ * Simple mnemonic-based implementation of the host Keystore interface.
+ */
+export class MnemonicKeystore implements Keystore {
+    readonly _brand = 'Keystore' as const;
+    private mnemonic: string;
+
+    constructor(mnemonic: string) {
+        this.mnemonic = mnemonic;
+    }
+
+    static random(): MnemonicKeystore {
+        const mnemonic = generateMnemonic(wordlist, 256);
+        return new MnemonicKeystore(mnemonic);
+    }
+
+    deriveAt(path: string): Hex.Hex {
+        const seed = mnemonicToSeedSync(this.mnemonic);
+        const root = HDKey.fromMasterSeed(seed);
+        const child = root.derive(path);
+
+        if (!child.privateKey) throw new Error(`Could not derive private key at path ${path}`);
+        return Hex.fromBytes(child.privateKey);
+    }
+}
